0c5894c5a63fcf1e3a2346a38a3634d82afea5ea
Ubuntu <ubuntu@ip-172-31-32-66.us-east-2.compute.internal>
2021-05-26 03:22:58 +0000

    complete google drive download script

--- backend/googleDriveDownload.sh
+++ backend/googleDriveDownload.sh
@@ -6,5 +6,3 @@ fi
 confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id='$1 -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
 echo $confirm
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$confirm&id=$1" -O $2 && rm -rf /tmp/cookies.txt
-
-# wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$1" -O $2 && rm -rf /tmp/cookies.txt
