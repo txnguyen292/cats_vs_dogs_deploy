79e1a8ccab00a641399cf17798f82882ff38898b
Ubuntu <ubuntu@ip-172-31-32-66.us-east-2.compute.internal>
2021-05-28 05:09:07 +0000

    FRONTEND. modify app.py for layout

--- frontend/app.py
+++ frontend/app.py
@@ -105,4 +105,4 @@ def update_output(n_clicks, contents):
         return html.H5(f"Prediction {pred['class']}")
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, port=8050)
